795f05de8dbaf5c0843a08f22f873ec922028b2a
Olivier Dolbeau <contact@odolbeau.fr>
2014-09-30 11:50:12 +0200

    Can now read credentials from ~/.influxdb_admin_cli

--- README.md
+++ README.md
@@ -12,11 +12,25 @@ You can download composer [here](https://getcomposer.org/download/).
 Once in the project you can run `./influxdb-admin-cli` to have a list of
 available commands.
 
+## Credentials
+
+You can create a file `~/.influxdb_admin_cli` to store your credentials. It
+looks like this:
+
+```json
+{"host":"my_influxdb","user":"admin","password":"p4ssw0rD"}
+```
+
+Here is the credentials importance order:
+
+* Specified in command call
+* `~/.influxdb_admin_cli` file if exist
+* default option provided by the command (prompt password as there is no default value)
+
 ## TODO
 
 * Use a .phar to be available to install this tool easily
 * Add more & more commands
-* Read credentials from a `~/.influxdb_admin_cli` if exist
 * Add the possibility to ask new passwords instead of passing it as arguments
 * @see composer to be able to selfupdate the command
 
--- src/Bab/InfluxDB/Command/Command.php
+++ src/Bab/InfluxDB/Command/Command.php
@@ -7,11 +7,11 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Bab\InfluxDB\InfluxDB;
+use Symfony\Component\Filesystem\Filesystem;
 
 class Command extends BaseCommand
 {
     protected $jsonOutput = false;
-
 
     /**
      * {@inheritDoc}
@@ -23,7 +23,7 @@ class Command extends BaseCommand
             ->addOption('port', null, InputOption::VALUE_REQUIRED, 'Which port?', 8086)
             ->addOption('user', 'u', InputOption::VALUE_REQUIRED, 'Which user?', 'root')
             ->addOption('password', 'p', InputOption::VALUE_REQUIRED, 'Which password? If nothing provided, password is asked', null)
-        ;
+            ;
 
         if ($this->jsonOutput) {
             $this->addOption('raw', null, InputOption::VALUE_NONE, 'Do not pretty print result', null);
@@ -40,18 +40,41 @@ class Command extends BaseCommand
      */
     protected function getInfluxDB(InputInterface $input, OutputInterface $output)
     {
-        $password = $input->getOption('password');
-        if (null === $password) {
+        $fs = new Filesystem();
+        $credentials = array();
+        $file = rtrim(getenv('HOME'), '/') . '/.influxdb_admin_cli';
+        if ($fs->exists($file)) {
+            $credentials = json_decode(file_get_contents($file), true);
+        }
+
+        if ($input->hasParameterOption(['--host', '-H'])) {
+            $host = $input->getOption('host');
+        } else {
+            $host = isset($credentials['host'])? $credentials['host'] : $input->getOption('host');
+        }
+
+        if ($input->hasParameterOption('--port')) {
+            $port = $input->getOption('port');
+        } else {
+            $port = isset($credentials['port'])? $credentials['port'] : $input->getOption('port');
+        }
+
+        if ($input->hasParameterOption(['--user', '-u'])) {
+            $user = $input->getOption('user');
+        } else {
+            $user = isset($credentials['user'])? $credentials['user'] : $input->getOption('user');
+        }
+
+        if ($input->hasParameterOption(['--password', '-p'])) {
+            $password = $input->getOption('password');
+        } elseif (isset($credentials['password'])) {
+            $password = $credentials['password'];
+        } elseif (null === $password = $input->getOption('password')) {
             $dialog = $this->getHelperSet()->get('dialog');
             $password = $dialog->askHiddenResponse($output, 'Password?', false);
         }
 
-        return new InfluxDB(
-            $input->getOption('host'),
-            $input->getOption('port'),
-            $input->getOption('user'),
-            $password
-        );
+        return new InfluxDB($host, $port, $user, $password);
     }
 
     /**
